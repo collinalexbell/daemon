f04b8dddcbf12cd7a45c730b0324689eae350163
kuberlog <collinalexbell@gmail.com>
2022-07-28 13:30:55 +0000

    main works

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
+import sys
 import torchaudio
 from speechbrain.pretrained import Tacotron2
 from speechbrain.pretrained import HIFIGAN
 import fileinput
-import sys
 
 # Intialize TTS (tacotron2) and Vocoder (HiFIGAN)
 tacotron2 = Tacotron2.from_hparams(source="speechbrain/tts-tacotron2-ljspeech", savedir="tmpdir_tts")
 hifi_gan = HIFIGAN.from_hparams(source="speechbrain/tts-hifigan-ljspeech", savedir="tmpdir_vocoder", run_opts={"device":"cuda"} )
 
+print("!", flush=True);
 count = 0
 for line in sys.stdin:
     # Running the TTS
@@ -19,7 +20,7 @@ for line in sys.stdin:
     waveforms = waveforms.squeeze(1).cpu()
 
     # Save the waverform
-    torchaudio.save(f'{count}.wav',waveforms, 22050)
-    print(count)
+    torchaudio.save(f'audio/{count}.wav',waveforms, 22050)
+    print(count, flush=True)
     count += 1
     count %= 100
